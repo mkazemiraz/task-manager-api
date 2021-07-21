98585197106210e04568b47b68443bf3584b2388
mkazemiraz <kazemiraz72@gmail.com>
2021-07-21 15:27:18 +0430

    updated

--- src/models/user.js
+++ src/models/user.js
@@ -10,11 +10,23 @@ const userSchema = mongoose.Schema({
         required: true,
         trim: true
     },
+    email: {
+        // type: String,
+        // required: true,
+        unique: false,
+        // trim: true,
+        // lowercase: true,
+        // validate(value) {
+        //     if (!validator.default.isEmail(value)) {
+        //         throw new Error('email is invalid');
+        //     }
+        // }
+    },
     password: {
         type: String,
         required: true,
         trim: true,
-        minlength: 10,
+        minlength: 7,
         validate(value) {
             if (value.toLowerCase().includes('password')) {
                 throw new Error('password cannot contain "password"');
@@ -44,66 +56,66 @@ const userSchema = mongoose.Schema({
 });
 
 
-// userSchema.virtual('tasks', {
-//     ref: 'Task',
-//     localField: '_id',
-//     foreignField: 'owner'
-// });
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+});
 
-// userSchema.methods.toJSON = function() {
-//     const user = this;
-//     const userPublic = user.toObject();
+userSchema.methods.toJSON = function() {
+    const user = this;
+    const userPublic = user.toObject();
 
-//     delete userPublic.password;
-//     delete userPublic.tokens;
-//     delete userPublic.avatar;
+    delete userPublic.password;
+    delete userPublic.tokens;
+    delete userPublic.avatar;
 
-//     return userPublic;
-// }
+    return userPublic;
+}
 
-// userSchema.methods.generateAuthToken = async function() {
-//     const user = this;
-//     const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {expiresIn: '7 days'});
-//     user.tokens = user.tokens.concat({token});
-//     await user.save();
+userSchema.methods.generateAuthToken = async function() {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {expiresIn: '7 days'});
+    user.tokens = user.tokens.concat({token});
+    await user.save();
 
-//     return token;
-// }
+    return token;
+}
 
 // findByCredential
-// userSchema.statics.findByCredential = async (email, password) => {
-//     const user = await User.findOne({email});
+userSchema.statics.findByCredential = async (email, password) => {
+    const user = await User.findOne({email});
     
-//     if (!user) {
-//         throw new Error('Unable to login');
-//     }
+    if (!user) {
+        throw new Error('Unable to login');
+    }
 
-//     isMatch = await bcryptjs.compare(password, user.password);
+    isMatch = await bcryptjs.compare(password, user.password);
 
-//     if (!isMatch) {
-//         throw new Error('password is incorrect');
-//     }
+    if (!isMatch) {
+        throw new Error('password is incorrect');
+    }
 
-//     return user;
-// }
+    return user;
+}
 
 // Hash password
-// userSchema.pre('save', async function(next) {
-//     const user = this;
-//     if (user.isModified('password')){
-//         user.password = await bcryptjs.hash(user.password, 8);
-//     }
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (user.isModified('password')){
+        user.password = await bcryptjs.hash(user.password, 8);
+    }
 
-//     next();
-// });
+    next();
+});
 
-// userSchema.pre('remove', async function(next) {
-//     const user = this;
+userSchema.pre('remove', async function(next) {
+    const user = this;
 
-//     await Task.deleteMany({ owner: user._id });
+    await Task.deleteMany({ owner: user._id });
 
-//     next();
-// });
+    next();
+});
 
 const User = mongoose.model('User', userSchema);
 
