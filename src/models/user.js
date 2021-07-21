b672bb75d9164f801f99b1e23c3634efa05155ec
mkazemiraz <kazemiraz72@gmail.com>
2021-07-21 14:30:44 +0430

    updated

--- src/models/user.js
+++ src/models/user.js
@@ -20,18 +20,18 @@ const userSchema = mongoose.Schema({
         required: true,
         trim: true
     },
-    email: {
-        type: String,
-        // required: true,
-        // unique: true,
-        trim: true,
-        // lowercase: true,
-        // validate(value) {
-        //     if (!validator.default.isEmail(value)) {
-        //         throw new Error('email is invalid');
-        //     }
-        // }
-    },
+    // email: {
+    //     type: String,
+    //     // required: true,
+    //     // unique: true,
+    //     trim: true,
+    //     // lowercase: true,
+    //     // validate(value) {
+    //     //     if (!validator.default.isEmail(value)) {
+    //     //         throw new Error('email is invalid');
+    //     //     }
+    //     // }
+    // },
     password: {
         type: String,
         // required: true,
